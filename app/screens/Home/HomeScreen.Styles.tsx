8af468be83ef0c01be83b8624d1e9671d890e963
Akinsanya Omogbolahan <akinsanya.omogbolahan@outlook.com>
2020-05-28 12:32:55 +0100

    Code refactor - removed unused React import reference

--- app/screens/Home/HomeScreen.Styles.tsx
+++ app/screens/Home/HomeScreen.Styles.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {StyleSheet} from 'react-native';
 import {Colours} from '../../constant/Colours';
 import {AppSectionColour} from '../../shared/layoutStyles/DarkLayoutStyle';
